7f66e428ce0bd25a3ed980eb27f6b0041e2af3ce
Sergey <rihlerstudy@gmail.com>
2025-03-13 21:04:17 +0300

    Добавление класса жильца дома, статического класса для рег. выр.

---
+++ coursework_oop/Tenant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursework_oop
+{
+    class Tenant
+    {
+        // Автоматические свойства
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int AppartamentNumb { get; set; }
+        public decimal Rent { get; set; }
+        public decimal Electricity { get; set; }
+        public decimal Utilities { get; set; }
+
+        // Конструктор
+        public Tenant(string lastName, string firstName, int appartamentNumb, decimal rent, decimal electricity, decimal utilities)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            AppartamentNumb = appartamentNumb;
+            Rent = rent;
+            Electricity = electricity;
+            Utilities = utilities;
+        }
+    }
+
+    static class Regs
+    {
+        public const String names = @"^[A-Z][a-z]*$";
+
+        public const String cost = @"^(?: [1 - 9]\d*|0)(?:\,\d+)?$";
+
+        public const String flatNumb = @"^(?:[1-9]\d*|0)$";
+    }
+}
